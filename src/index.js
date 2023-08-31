880fae8b7cde4cd7ac9401a423aa13ab38d60009
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2023-08-31 22:32:12 +0200

    Add updated index.js file

--- src/index.js
+++ src/index.js
@@ -1,2 +1,33 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import './style.css';
+import Data from './modules/inputObj.js';
+import submitScore from './modules/submitScore.js';
+import fetchScore from './modules/fetchScore.js';
+
+const formData = document.getElementById('form-box');
+const refreshBTN = document.getElementById('refresh-btn');
+const scoreList = document.getElementById('table');
+
+formData.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const userInput = document.getElementById('playerName');
+  const scoreInput = document.getElementById('points');
+  const user = userInput.value;
+  const score = scoreInput.value;
+  const newData = new Data(user, score);
+
+  await submitScore(newData);
+  document.getElementById('playerName').value = '';
+  document.getElementById('points').value = '';
+});
+
+refreshBTN.addEventListener('click', () => {
+  scoreList.innerHTML = '';
+  fetchScore();
+});
+
+window.addEventListener('load', () => {
+  scoreList.innerHTML = '';
+  fetchScore();
+});
